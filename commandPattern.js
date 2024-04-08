51b8ac7b4027582782e0e9c2b4a9aa466bac2347
AlejoGomezQ <alejandro.gomezq97@gmail.com>
2024-04-08 17:49:23 -0500

    Command pattern added

---
+++ commandPattern.js
@@ -0,0 +1,92 @@
+class Calculator {
+  constructor() {
+    this.value = 0;
+    this.history = [];
+  }
+
+  executeCommand(command) {
+    this.value = command.execute(this.value);
+    this.history.push(command);
+  }
+
+  undoCommand() {
+    const undoCommand = this.history.pop();
+    this.value = undoCommand.undo(this.value);
+  }
+}
+
+class AddCommand {
+  constructor(valueToAdd) {
+    this.valueToAdd = valueToAdd;
+  }
+
+  execute(currentValue) {
+    return currentValue + this.valueToAdd;
+  }
+
+  undo(currentValue) {
+    return currentValue - this.valueToAdd;
+  }
+}
+
+class SubtractCommand {
+  constructor(valueToSubtract) {
+    this.valueToSubtract = valueToSubtract;
+  }
+
+  execute(currentValue) {
+    return currentValue - this.valueToSubtract;
+  }
+
+  undo(currentValue) {
+    return currentValue + this.valueToSubtract;
+  }
+}
+
+class DivideCommand {
+  constructor(valueToDivide) {
+    this.valueToDivide = valueToDivide;
+  }
+
+  execute(currentValue) {
+    return currentValue / this.valueToDivide;
+  }
+
+  undo(currentValue) {
+    return currentValue * this.valueToDivide;
+  }
+}
+
+class MultiplyCommand {
+  constructor(valueToMultiply) {
+    this.valueToMultiply = valueToMultiply;
+  }
+
+  execute(currentValue) {
+    return currentValue * this.valueToMultiply;
+  }
+
+  undo(currentValue) {
+    return currentValue / this.valueToMultiply;
+  }
+}
+
+class AddThenMultiplyCommand {
+  constructor(valueToAdd, valueToMultiply) {
+    this.addCommand = new AddCommand(valueToAdd);
+    this.multiplyCommand = new MultiplyCommand(valueToMultiply);
+  }
+
+  execute(currentValue) {
+    const newValue = this.addCommand.execute(currentValue);
+    return this.multiplyCommand.execute(newValue);
+  }
+
+  undo(currentValue) {
+    const newValue = this.multiplyCommand.undo(currentValue);
+    return this.addCommand.undo(newValue);
+  }
+}
+
+const calculator = new Calculator();
+calculator.executeCommand(new AddCommand(10));
